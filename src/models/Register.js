c06f16993e64b592b1afbca3ce6a8f7cfa4407dc
jpzapata99 <jzapat90@eafit.edu.co>
2020-01-23 10:05:33 -0500

    Añadiendo consultas basicas get y post

---
+++ src/models/Register.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose;
+
+const Register = new Schema({
+    nombre: {type:String, required:true},
+    temperatura: {type:Number, required:true},
+    humedad: {type:Number, required:true},
+    longitud : {type:Number, required:true},
+    latitud : {type:Number, required:true}
+})
+
+module.exports = mongoose.model('Register',Register)
--- src/routes/registers.routes.js
+++ src/routes/registers.routes.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const router = express.Router(); 
+const Register = require ('./../models/Register')
 
+router.get('/',async (req,res)=>{
+    const registers =  await Register.find()
+    console.log(registers)
+    res.json(registers)   
+})
 
-router.get('/',(req,res)=>{
-    res.send("Hola Mundo")
+router.post('/', async (req,res)=>{
+    const { nombre, temperatura, humedad,longitud, latitud} = req.body
+    const register = new Register({nombre, temperatura, humedad,longitud, latitud});
+    console.log(register)
+    await register.save()
+    res.json("recibido")
 })
 
 
